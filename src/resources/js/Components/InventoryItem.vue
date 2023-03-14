260eb921863682ef65417e64c3b4c1b32479992f
ge-vi <gerardas.virsuta@gmail.com>
2023-03-14 20:56:40 +0200

    Refactor InventoryItem so SimpleCard is used

--- src/resources/js/Components/InventoryItem.vue
+++ src/resources/js/Components/InventoryItem.vue
@@ -1,30 +1,25 @@
 <script setup>
-import { router } from '@inertiajs/vue3';
-
-import { useForm, Link } from '@inertiajs/vue3';
-import { ref } from 'vue';
-import Dropdown from '@/Components/Dropdown.vue';
-import DropdownLink from '@/Components/DropdownLink.vue';
-import PrimaryButton from '@/Components/PrimaryButton.vue';
-import InputError from '@/Components/InputError.vue';
+import {router} from '@inertiajs/vue3';
+import SimpleCard from "@/Components/BS/SimpleCard.vue";
 
 const props = defineProps(['item']);
 
 const deleteItem = () => {
-  router.delete(route('items.destroy', props.item.id), {
-        onBefore: () => confirm('Are you sure you want to delete this item?'),
-    });
+  router.delete(
+    route('items.destroy', props.item.id),
+    {
+    onBefore: () => confirm('Are you sure you want to delete this item?'),
+  });
 };
 const editItem = () => {
   router.get(route('items.edit', props.item.id));
 };
-
 </script>
 
 <template>
   <div class="col gy-3">
-    <div class="card shadow">
-      <div class="card-header d-flex justify-content-between">
+    <SimpleCard>
+      <template #header>
         <span>
           <small class="text-muted">item type:</small><br>
           <h3>
@@ -66,29 +61,33 @@ const editItem = () => {
             </li>
           </ul>
         </div>
-      </div>
+      </template>
 
-      <ul class="list-group list-group-flush">
-        <li class="list-group-item">
-          <small class="text-muted">name:<br></small>
-          {{ item?.name }}
-        </li>
-        <li class="list-group-item">
-          <small class="text-muted">description:<br></small>{{ item?.description }}
-        </li>
-        <li class="list-group-item">
-          <small class="text-muted">warranty:<br></small>{{ item?.warranty_months }} months
-        </li>
-        <li class="list-group-item">
-          <small class="text-muted">EOL:<br></small>{{ item?.type?.period - item?.warranty_months }} months
-        </li>
-        <li class="list-group-item">
-          <small class="text-muted">owner's name<br></small>{{ item?.owner?.name }}
-        </li>
-        <li class="list-group-item">
-          <small class="text-muted">owner's email:<br></small>{{ item?.owner?.email }}
-        </li>
-      </ul>
-    </div>
+      <template #body>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">
+            <small class="text-muted">name:<br></small>
+            {{ item?.name }}
+          </li>
+          <li class="list-group-item">
+            <small class="text-muted">description:<br></small>{{ item?.description }}
+          </li>
+          <li class="list-group-item">
+            <small class="text-muted">warranty:<br></small>{{ item?.warranty_months }} months
+          </li>
+          <li class="list-group-item">
+            <small class="text-muted">EOL:<br></small>{{ item?.type?.period - item?.warranty_months }} months
+          </li>
+          <li class="list-group-item">
+            <small class="text-muted">owner's name<br></small>{{ item?.owner?.name }}
+          </li>
+          <li class="list-group-item">
+            <small class="text-muted">owner's email:<br></small>{{ item?.owner?.email }}
+          </li>
+        </ul>
+      </template>
+
+    </SimpleCard>
   </div>
+  <!--col-->
 </template>
